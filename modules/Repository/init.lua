fe2bd05c18515ed1abcd228d48b2777ddb3b354a
Emil Rasmus Merzin <rasmus@merzin.dev>
2023-05-14 14:06:06 +0300

    repository docs

--- modules/Repository/init.lua
+++ modules/Repository/init.lua
@@ -2,11 +2,54 @@ local DataStoreService = game:GetService("DataStoreService")
 local Players = game:GetService("Players")
 local Timer = require(script.Parent.Timer)
 
+--[=[
+	@interface Entity
+	.Start (self: Entity, player: Player?) -> nil
+	@within Repository
+]=]
+type Entity = {
+	Start: (self: Entity, player: Player?) -> nil,
+}
+
+--[=[
+	@class Repository
+
+	The Repository class is a DataStore wrapper which handles storing and loading.
+
+	```lua
+	local ProfileRepository = Repository.new({ Name = "Profiles" })
+
+	function ProfileRepository.Entity:Start(player: Player?)
+		if player then self.DisplayName = player.DisplayName end
+	end
+	```
+]=]
 local Repository = {
 	Name = "Global",
 }
 Repository.__index = Repository
 
+--[=[
+	@within Repository
+	@prop Name string
+	`DataStore` name to use when accessing `DataStoreService`.
+]=]
+--[=[
+	@within Repository
+	@prop Interval number
+	Interval at which connected players' entities are saved.
+]=]
+--[=[
+	@within Repository
+	@prop Entity Entity
+	Entity class used when initializing players' entities.
+]=]
+
+--[=[
+	@return Repository
+	@param init Repository?
+	Creates a `Repository`.
+]=]
 function Repository.new(init)
 	local this = setmetatable(init or {}, Repository)
 	this.Entity = this.Entity or {}
@@ -14,21 +57,45 @@ function Repository.new(init)
 	function this.Entity:Start() end
 	this.Store = DataStoreService:GetDataStore(this.Name)
 	this.Cache = {} :: Table<string, Profile>
-	Players.PlayerAdded:Connect(function(player: Player) this:OnPlayer(player, true) end)
-	Players.PlayerRemoving:Connect(function(...) this:OnPlayer(...) end)
+	Players.PlayerAdded:Connect(function(player: Player) this:_OnPlayer(player, true) end)
+	Players.PlayerRemoving:Connect(function(...) this:_OnPlayer(...) end)
 	this.Timer = Timer.new(init.Interval or 60)
-	this.Timer.Tick:Connect(function() this:OnTick() end)
+	this.Timer.Tick:Connect(function() this:_OnTick() end)
 	this.Timer:Start()
 	return this
 end
 
-function Repository:OnTick()
-	for _, player in pairs(Players:GetPlayers()) do
-		self:OnPlayer(player)
+--[=[
+	Gets an `Entity`.
+]=]
+function Repository:Get(id: number | string | Player): Entity
+	local player
+	if type(id) == "number" then
+		id = tostring(id)
+		player = Players:GetPlayerByUserId(id)
+	elseif type(id) == "string" then
+		player = Players:GetPlayerByUserId(tonumber(id) or 0)
+	else
+		id = tostring(id.UserId)
+		player = id
+	end
+	local cache = self.Cache[id]
+	if cache then
+		return cache
+	else
+		self.Cache[id] = setmetatable(self.Store:GetAsync(id) or {}, self.Entity)
+		self.Cache[id]:Start(player)
+		return self.Cache[id]
 	end
 end
 
-function Repository:OnPlayer(player: Player, forceUpdate: boolean?)
+function Repository:_OnTick()
+	for _, player in pairs(Players:GetPlayers()) do
+		self:_OnPlayer(player)
+	end
+end
+
+function Repository:_OnPlayer(player: Player, forceUpdate: boolean?)
 	local id = tostring(player.UserId)
 	local cache = self.Cache[id]
 	if cache and not forceUpdate then
@@ -42,25 +109,6 @@ function Repository:OnPlayer(player: Player, forceUpdate: boolean?)
 	else
 		self.Cache[id] = setmetatable(self.Store:GetAsync(id) or {}, self.Entity)
 		self.Cache[id]:Start(player)
-	end
-end
-
-function Repository:Get(id: number | string | Player)
-	local player
-	if type(id) == "number" then
-		id = tostring(id)
-		player = Players:GetPlayerByUserId(id)
-	elseif type(id) ~= "string" then
-		id = tostring(id.UserId)
-		player = id
-	end
-	local cache = self.Cache[id]
-	if cache then
-		return cache
-	else
-		self.Cache[id] = setmetatable(self.Store:GetAsync(id) or {}, self.Entity)
-		self.Cache[id]:Start(player)
-		return self.Cache[id]
 	end
 end
 
