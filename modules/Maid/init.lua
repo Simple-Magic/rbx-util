d9dfd80f04f57bb8a5b3fbf4f3ab73a9357a70ea
Emil Rasmus Merzin <rasmus@merzin.dev>
2023-05-14 14:58:46 +0300

    Maid docs

--- modules/Maid/init.lua
+++ modules/Maid/init.lua
@@ -1,13 +1,29 @@
+--[=[
+	@class Maid
+]=]
 local Maid = {}
 Maid.__index = Maid
 
-function Maid.new(): Maid return setmetatable({ _Items = {} :: { Variant } }, Maid) end
+--[=[
+	Creates `Maid` instance.
+]=]
+function Maid.new(): Maid
+	return setmetatable({
+		_Items = {} :: { Variant },
+	}, Maid)
+end
 
+--[=[
+	Add item to array.
+]=]
 function Maid:Add(item: Variant): Variant
 	table.insert(self._Items, item)
 	return item
 end
 
+--[=[
+	Add array of items to array.
+]=]
 function Maid:Append(items: { Variant }): { Variant }
 	for _, item in pairs(items) do
 		self:Add(item)
@@ -15,12 +31,16 @@ function Maid:Append(items: { Variant }): { Variant }
 	return items
 end
 
+--[=[
+	Cleanup array.
+]=]
 function Maid:Destroy()
 	for _, item in pairs(self._Items) do
 		pcall(function() item:Cancel() end)
 		pcall(function() item:Disconnect() end)
 		pcall(function() item:Destroy() end)
 	end
+	self._Items = {}
 end
 
 return Maid
