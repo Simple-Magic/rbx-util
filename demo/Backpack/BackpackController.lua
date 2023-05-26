2981bc7147dee990285719b83fb57db747704548
U-EXPLORE\erm <rasmus@merzin.dev>
2023-05-26 13:43:27 +0300

    backpack number keybinds

--- demo/Backpack/BackpackController.lua
+++ demo/Backpack/BackpackController.lua
@@ -1,11 +1,27 @@
 local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local StarterGui = game:GetService("StarterGui")
+local UserInputService = game:GetService("UserInputService")
 local Knit = require(ReplicatedStorage.Packages.Knit)
 local Timer = require(ReplicatedStorage.Packages.Timer)
 local Player = Players.LocalPlayer
 
 StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Backpack, false)
+
+function getInputNumber(inputObject: InputObject)
+	return ({
+		[Enum.KeyCode.One] = 1,
+		[Enum.KeyCode.Two] = 2,
+		[Enum.KeyCode.Three] = 3,
+		[Enum.KeyCode.Four] = 4,
+		[Enum.KeyCode.Five] = 5,
+		[Enum.KeyCode.Six] = 6,
+		[Enum.KeyCode.Seven] = 7,
+		[Enum.KeyCode.Eight] = 8,
+		[Enum.KeyCode.Nine] = 9,
+		[Enum.KeyCode.Zero] = 10,
+	})[inputObject.KeyCode]
+end
 
 local BackpackController = Knit.CreateController({
 	Name = "BackpackController",
@@ -20,7 +36,19 @@ function BackpackController:KnitInit()
 end
 
 function BackpackController:KnitStart()
-	Timer.Simple(0.2, function() self:OnTick() end, true)
+	Timer.Simple(0.1, function() self:OnTick() end, true)
+	UserInputService.InputEnded:Connect(function(...) self:OnInputEnded(...) end)
+end
+
+function BackpackController:OnInputEnded(inputObject: InputObject, processed: boolean)
+	if processed then return end
+	local inputNumber = getInputNumber(inputObject)
+	if inputNumber then
+		local tools = self:GetTools()
+		local tool = tools[inputNumber]
+		if not tool then return end
+		self:Toggle(tool)
+	end
 end
 
 function BackpackController:GetTools(): { Tool }
@@ -50,15 +78,26 @@ end
 
 function BackpackController:Equip(tool: Tool)
 	if not Player.Character then return end
+	if tool.Parent == Player.Character then return end
 	local equipped = Player.Character:FindFirstChildWhichIsA("Tool")
-	if equipped then equipped.Parent = Player.Backpack end
+	if equipped then self:Unequip(equipped) end
 	tool.Parent = Player.Character
+end
+
+function BackpackController:Unequip(tool: Tool) tool.Parent = Player.Backpack end
+
+function BackpackController:Toggle(tool: Tool)
+	if tool.Parent == Player.Character then
+		self:Unequip(tool)
+	else
+		self:Equip(tool)
+	end
 end
 
 function BackpackController:Add(tool: Tool, index: number?)
 	if not self.Buttons[tool] then
 		self.Buttons[tool] = self.ButtonTemplate:Clone()
-		self.Buttons[tool].MouseButton1Click:Connect(function() self:Equip(tool) end)
+		self.Buttons[tool].MouseButton1Click:Connect(function() self:Toggle(tool) end)
 		self.Buttons[tool].Parent = self.Gui.Frame
 	end
 	local equipped = tool.Parent == Player.Character
